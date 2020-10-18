5e0133961b26ef944118f87a6d34fbf8937cca4c
Slevkelebr <slevkelebr@yandex.ru>
2020-10-18 12:35:51 +0500

    Загрузка деталей поста.(Рефакторинг)

--- src/main/java/ru/job4j/parse/FormatDate.java
+++ src/main/java/ru/job4j/parse/FormatDate.java
@@ -4,6 +4,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,13 +13,34 @@ import java.util.regex.Pattern;
  * Форматирование даты.
  *
  * @author Sergey Frolov
- * @version 1.0
- * @since 17.10.2020
+ * @version 2.0
+ * @since 18.10.2020
  */
 
 public class FormatDate {
 
-    private final Pattern pattern = Pattern.compile("\\d{2}\\s\\D{3}\\s\\d{2}.\\s\\d{2}.\\d{2}");
+    private final SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yy, HH:mm");
+
+    private final Calendar today = Calendar.getInstance();
+
+    private final Pattern pattern = Pattern.compile("\\d{1,2}\\s\\D{3}\\s\\d{2}.\\s\\d{2}.\\d{2}");
+
+    private final Map<String, String> monthMapper = new HashMap<>() {
+        {
+            put("янв", "янв.");
+            put("фев", "февр.");
+            put("мар", "мар.");
+            put("апр", "апр.");
+            put("май", "май");
+            put("июн", "июн.");
+            put("июл", "июл.");
+            put("авг", "авг.");
+            put("сен", "сент.");
+            put("окт", "окт.");
+            put("ноя", "нояб.");
+            put("дек", "дек.");
+        }
+    };
 
     /**
      * Класс сопоставляет строку для получения даты в формате {@code dd MMM yy, HH:mm}.
@@ -40,8 +63,19 @@ public class FormatDate {
      */
     public String formatDate(String oldDate) throws ParseException {
         String newDate = replaceMonthOrDate(oldDate);
-        Date date = new SimpleDateFormat("dd MMM yy, HH:mm").parse(newDate);
+        Date date = formatter.parse(newDate);
         return new SimpleDateFormat("dd-MM-yyyy HH:mm").format(date);
+    }
+
+    private String parseToday() {
+        Date day = today.getTime();
+        return formatter.format(day);
+    }
+
+    private String parseYesterday() {
+        today.add(Calendar.DAY_OF_YEAR, -1);
+        Date yesterday = today.getTime();
+        return formatter.format(yesterday);
     }
 
     /**
@@ -50,29 +84,20 @@ public class FormatDate {
      * @return дата с отформатированным месяцем.
      */
     private String replaceMonthOrDate(String date) {
-        Date today = new Date();
-        Calendar day = Calendar.getInstance();
-        SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yy, HH:mm");
+        Map<String, String> mapper = new HashMap<>() {
+            {
+                put("сегодня,", parseToday());
+                put("вчера,", parseYesterday());
+            }
+        };
         String[] str = date.split(" ");
-        if (str[0].contains("сегодня")) {
-            str[0] = formatter.format(today);
-            str[1] = "";
-        } else if (str[0].contains("вчера")) {
-            day.add(Calendar.DAY_OF_YEAR, -1);
-            Date yesterday = day.getTime();
-            str[0] = formatter.format(yesterday);
-            str[1] = "";
-        } else if (str[1].equals("сен")) {
-            str[1] = "сент.";
-        } else if (str[1].equals("май")) {
-            return date;
-        } else if (str[1].equals("ноя")) {
-            str[1] = "нояб.";
-        } else if (str[1].equals("фев")) {
-            str[1] = "февр.";
-        } else {
-            str[1] = str[1] + ".";
+        if (monthMapper.containsKey(str[1])) {
+            str[1] = monthMapper.get(str[1]);
+        }
+        if (mapper.containsKey(str[0])) {
+            str[0] = String.valueOf(mapper.get(str[0]));
         }
         return String.join(" ", str);
     }
+
 }
