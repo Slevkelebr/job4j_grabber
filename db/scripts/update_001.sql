30c6633c7bbe900b27f24e51eb37adcb42640c34
Slevkelebr <Slevkelebr@yandex.ru>
2020-10-27 12:29:59 +0500

    Grabber(Интеграционные тесты обновление схемы для ликвибейз)

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,4 +1,7 @@
-create table rabbit (
-    id serial primary key not null,
-    created_date date
+create table post (
+                      id serial primary key,
+                      name varchar(255),
+                      text text,
+                      link varchar(255) unique,
+                      created timestamp
 );
